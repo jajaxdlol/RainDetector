f9183248d46ff1ea7f82a6dc5433d89bc1c05586
Miguel Pérez <miguel_on_94@hotmail.com>
2018-03-25 21:20:12 +0200

    Low power added to TX fixed

--- transmisor/transmisor.ino
+++ transmisor/transmisor.ino
@@ -7,8 +7,8 @@ const int rainSensor = 0;
 int sensor = 0;
 int constantCicles = 0;
 int oldData = 0;
-char oseg[VW_MAX_MESSAGE_LEN];
-char cseg[VW_MAX_MESSAGE_LEN];
+const char* oseg = "l";
+const char* cseg = "s";
 
 void setup()
 {
@@ -16,11 +16,6 @@ void setup()
   vw_setup(2000);
   vw_set_tx_pin(dataPin);
   oldData = analogRead(rainSensor);
-  String sleepMsg;
-  sleepMsg = "p" + 8;
-  sleepMsg.toCharArray(oseg,sizeof(oseg));
-  sleepMsg = "p" + 4;
-  sleepMsg.toCharArray(cseg,sizeof(cseg));
 }
 
 void loop()
@@ -38,17 +33,11 @@ void loop()
   vw_send((uint8_t *)buf, strlen(buf));
   vw_wait_tx();
   LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
-  Serial.print("LECTURA SENSOR: ");
-  Serial.println(buf);
-  Serial.print("LECTURA ANTIGUA: ");
-  Serial.println(oldData);
   if (constantCicles >= 40) {
-    Serial.println("8 segundos");
     vw_send((uint8_t *)oseg, strlen(oseg));
     vw_wait_tx();
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
   } else if (constantCicles >= 20) {
-    Serial.println("4 segundos");
     vw_send((uint8_t *)cseg, strlen(cseg));
     vw_wait_tx();
     LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
